72d536a568a8dfdb534c6e98cdb805c47325d720
veysel <vinanc72@gmail.com>
2025-04-20 20:41:01 +0300

    Update tsstatus.js

--- jobs/tsstatus.js
+++ jobs/tsstatus.js
@@ -5,7 +5,7 @@ async function pingTeknoSeyir() {
     try {
         const response = await axios.get('https://teknoseyir.com/', {
             headers: {
-                'User-Agent': 'GitHubActionsBot/1.0 (https://github.com/tsopenteam/content)',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Referer': 'https://teknoseyir.com/'
             }
         });
